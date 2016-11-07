7e2ab9db026eee45fa7007d48f7457cbbc20dba9
swxy <swxymile@163.com>
2016-11-07 22:06:55 +0800

    add header

--- app.js
+++ app.js
@@ -1,7 +1,95 @@
 const http = require('http');
 const Socket = require('./socks5/socket');
 const net = require('net');
+const url = require('url');
 
+let defautOption = {
+    socksPort: 1080,
+    socksHost: '127.0.0.1',
+    method: 'GET',
+    createConnection: (options, callback) => {
+        return new Socket(options).connect(options, callback);
+    }
+};
+
+function capitalizeFirstLetter (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function keyToUpperCase (key) {
+    if (!key) {
+        return key;
+    }
+    let keys = key.split('-');
+    for (let i = 0; i < keys.length; i++) {
+        keys[i] = capitalizeFirstLetter(keys[i]);
+    }
+    return keys.join('-');
+}
+
+function proxyRequest (option, res, data) {
+    console.log(option);
+    let proxy = http.request(option, (response) => {
+        console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+        //res.writeHead(200, response.headers);
+        for (let key in response.headers) {
+            if (response.headers.hasOwnProperty(key)) {
+                res.setHeader(keyToUpperCase(key), response.headers[key]);
+            }
+        }
+        res.writeHead(response.statusCode);
+        //res.setHeader(response.headers);
+        response.on('data', (chunk) => {
+            res.write(chunk);
+        });
+        response.on('end', () => {
+            res.end();
+        });
+    });
+
+    proxy.on('error', (e) => {
+        console.error(e);
+    });
+    console.log(data && data.toString());
+    data && proxy.write(data);
+    proxy.end();
+}
+
+http.createServer((req, res) => {
+    console.log(req.url);
+    let urlObj = url.parse(req.url);
+    let headers = req.headers;
+    let option = Object.assign({}, defautOption, {
+        host: req.headers.host,
+        method: req.method,
+        port: 80,
+        path: urlObj.path,
+        headers: {}
+    });
+    for (let header in headers) {
+        if (headers.hasOwnProperty(header)) {
+            option.headers[keyToUpperCase(header)] = headers[header];
+        }
+    }
+    console.log('method: ' + req.method);
+    console.log(req.headers);
+    if (req.method.toLocaleLowerCase() === 'post') {
+        let body = '';
+        req.on('data', (data) => {
+            body += data;
+        });
+        req.on('end', () => {
+            proxyRequest(option, res, body);
+        })
+    }
+    else {
+        proxyRequest(option, res);
+    }
+}).listen(1338, () => {
+    console.log('local server at 1338');
+});
+
+/*
 var option = {
     socksPort: 1337,
     socksHost: '127.0.0.1',
@@ -28,3 +116,4 @@ req.on('error', (e) => {
 });
 
 req.end();
+*/
